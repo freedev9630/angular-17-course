d66f97ad6c53ff71081ce97181bbf1e97b6d06b3
Fre dev <freedev9630@gmail.com>
2024-04-07 00:36:50 -0500

    feat(feature/basic-config) basic config

---
+++ src/app/pages/todo/todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss'
+})
+export class TodoComponent {}
---
+++ src/assets/todo.ts
@@ -0,0 +1,146 @@
+export const TODO_DATA = [
+  {
+    "title": "Leer documentación técnica",
+    "description": "Investigar y leer la documentación de una nueva tecnología o herramienta relevante para el proyecto.",
+    "status": "Por hacer",
+    "priority": 3,
+    "hidden": false,
+    "id": 1,
+    "deadLine": "2024-04-07T03:25:54.898Z",
+    "color": {
+      "status": "#ed4040",
+      "priority": "#68db68"
+    },
+    "class": {
+      "status": "to-do",
+      "priority": "low"
+    },
+    "progress": 0.2
+  },
+  {
+    "title": "Resolver un bug",
+    "description": "Identificar y corregir un problema de software reportado por el equipo de pruebas.",
+    "status": "En progreso",
+    "priority": 2,
+    "hidden": false,
+    "id": 2,
+    "deadLine": "2024-04-08T03:25:54.898Z",
+    "color": {
+      "status": "#76761c",
+      "priority": "#3e3e8f"
+    },
+    "class": {
+      "status": "in-progress",
+      "priority": "medium"
+    },
+    "progress": 0.5
+  },
+  {
+    "title": "Optimizar el rendimiento del código",
+    "description": "Realizar análisis y ajustes en el código para mejorar su eficiencia y rendimiento.",
+    "status": "En progreso",
+    "priority": 1,
+    "hidden": false,
+    "id": 3,
+    "deadLine": "2024-04-09T03:25:54.898Z",
+    "color": {
+      "status": "#76761c",
+      "priority": "#ed4040"
+    },
+    "class": {
+      "status": "in-progress",
+      "priority": "high"
+    },
+    "progress": 0.75
+  },
+  {
+    "title": "Revisar y responder correos de trabajo",
+    "description": "Dedicar 15 minutos a revisar y responder correos electrónicos relacionados con el proyecto.",
+    "status": "Hecho",
+    "priority": 3,
+    "hidden": false,
+    "id": 4,
+    "deadLine": "2024-04-22T03:25:54.898Z",
+    "color": {
+      "status": "#68db68",
+      "priority": "#68db68"
+    },
+    "class": {
+      "status": "done",
+      "priority": "low"
+    },
+    "progress": 1
+  },
+  {
+    "title": "Preparar una presentación técnica",
+    "description": "Elaborar una presentación para compartir avances o resultados del proyecto con el equipo.",
+    "status": "Por hacer",
+    "priority": 1,
+    "hidden": false,
+    "id": 5,
+    "deadLine": "2024-04-18T03:25:54.898Z",
+    "color": {
+      "status": "#ed4040",
+      "priority": "#ed4040"
+    },
+    "class": {
+      "status": "to-do",
+      "priority": "high"
+    },
+    "progress": 0.1
+  },
+  {
+    "title": "Realizar una llamada de seguimiento con el equipo",
+    "description": "Comunicarse con un compañero de equipo para discutir el progreso de una tarea o resolver un problema.",
+    "status": "Por hacer",
+    "priority": 2,
+    "hidden": false,
+    "id": 6,
+    "deadLine": "2024-04-12T03:25:54.898Z",
+    "color": {
+      "status": "#ed4040",
+      "priority": "#76761c"
+    },
+    "class": {
+      "status": "to-do",
+      "priority": "medium"
+    },
+    "progress": 0.3
+  },
+  {
+    "title": "Revisar y optimizar el uso de recursos en la nube",
+    "description": "Evaluar y ajustar la configuración de recursos en la nube para optimizar el rendimiento y reducir costos.",
+    "status": "Por hacer",
+    "priority": 1,
+    "hidden": false,
+    "id": 7,
+    "deadLine": "2024-04-28T03:25:54.898Z",
+    "color": {
+      "status": "#ed4040",
+      "priority": "#ed4040"
+    },
+    "class": {
+      "status": "to-do",
+      "priority": "high"
+    },
+    "progress": 0.4
+  },
+  {
+    "title": "Refactorizar código legacy",
+    "description": "Dedicar tiempo a reestructurar y mejorar el código antiguo para que sea más mantenible y escalable.",
+    "status": "Backlog",
+    "priority": 3,
+    "hidden": false,
+    "id": 8,
+    "deadLine": "2024-04-14T03:25:54.898Z",
+    "color": {
+      "status": "#808080",
+      "priority": "#68db68"
+    },
+    "class": {
+      "status": "backlog",
+      "priority": "low"
+    },
+    "progress": 0.6
+  }
+];
